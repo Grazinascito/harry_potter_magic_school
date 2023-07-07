e9f50d099cc5eb61573100cf9a61b9a6ccac6730
grazinascito <grazi.social84@gmail.com>
2023-07-07 12:04:33 -0300

    feat: add content card

---
+++ lib/components/content_card.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import '../../global_style.dart';
+
+class ContentCard extends StatelessWidget {
+  const ContentCard(
+      {super.key,
+      required this.titleCard,
+      required this.subText,
+      required this.icon,
+      required this.image});
+
+  final String titleCard;
+  final String subText;
+  final String icon;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(.2),
+          blurRadius: 25.0, // soften the shadow
+          spreadRadius: 0.0, //extend the shadow
+        )
+      ]),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Card(
+              color: whiteDefault,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Image.asset(
+                        image,
+                        width: 100,
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 220,
+                          child: ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            leading: Image.asset(icon),
+                            title: Text(
+                              titleCard,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 17),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 220,
+                          child: Text(
+                            subText,
+                            style: const TextStyle(fontSize: 17),
+                          ),
+                        ),
+                        const SizedBox(height: 13),
+                        SizedBox(
+                          height: 34,
+                          child: ElevatedButton(
+                              onPressed: () => print("oiii"),
+                              style: ElevatedButton.styleFrom(
+                                  fixedSize: const Size(220, 0),
+                                  backgroundColor: redDefault,
+                                  foregroundColor: whiteDefault,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(4))),
+                              child: const DefaultTextStyle(
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                child: Text(
+                                  "Acessar",
+                                  textAlign: TextAlign.center,
+                                ),
+                              )),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
